c4a7f897fbb650d3d7ae3a39b8f5f8186989e7e6
Mark <fletinrok@gmail.com>
2020-09-15 19:33:13 +0700

    show all ducks

--- main.cpp
+++ main.cpp
@@ -3,16 +3,35 @@
 #include "mallardduck.h"
 #include "redheadduck.h"
 #include "rubberduck.h"
+#include "decoyduck.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-
     Duck *d1 = new RedHeadDuck();
     d1->display();
     d1->PerformQuack();
     d1->PerformFly();
+    cout << "==========" << endl;
+
+   Duck *d2 = new MallardDuck();
+   d2->display();
+   d2->PerformQuack();
+   d2->PerformFly();
+   cout << "==========" << endl;
+
+   Duck *d3 = new RubberDuck();
+   d3->display();
+   d3->PerformQuack();
+   d3->PerformFly();
+   cout << "==========" << endl;
+
+   Duck *d4 = new DecoyDuck();
+   d4->display();
+   d4->PerformQuack();
+   d4->PerformFly();
+   cout << "==========" << endl;
 
     return a.exec();
 }
